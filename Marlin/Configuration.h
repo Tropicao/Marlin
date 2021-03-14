242914e5336e29e64da7d09b4ba190f1d1aae97e
Alexis Lothoré <alexis.lothore@gmail.com>
2021-03-14 22:15:10 +0100

    Configure PID for bed and nozzle temperature

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -512,9 +512,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  25.19
+    #define DEFAULT_Ki   1.90
+    #define DEFAULT_Kd  83.32
   #endif
 #endif // PIDTEMP
 
@@ -551,9 +551,9 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  #define DEFAULT_bedKp 50.71
-  #define DEFAULT_bedKi 9.88
-  #define DEFAULT_bedKd 173.43
+  #define DEFAULT_bedKp 112.60
+  #define DEFAULT_bedKi 19.25
+  #define DEFAULT_bedKd 439.13
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
